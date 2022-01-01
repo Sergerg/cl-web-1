5a495d9a3eef233bc9382ffe7ed60ac048c4a6d5
Sergey Galichanin <sergerg@gmail.com>
2022-01-01 21:16:01 +0400

    Вынес в функцию

--- src/cl_easy/twosum.clj
+++ src/cl_easy/twosum.clj
@@ -11,6 +11,23 @@
   [1 1]
   )
 
+(defn bbb [lst1 size sum]
+  (let [first1 (first lst1)]
+    (loop [lst2 (rest lst1)]
+      (let [pos1 (- size (count lst1))
+            pos2 (- size (count lst2))
+            flag (not= sum (+ first1 (first lst2)))]
+        (println "   > pos1=" pos1  ;; 1-я позиция
+                 "pos2=" pos2     ;; 2-я позиция
+                 "flag=" flag
+                 )
+        (when (and lst2 flag)
+          (println "- =" lst2
+                   (count lst2)
+                   "first1=" first1)
+          (recur (next lst2)))))
+    ))
+
 ;; TEST!
 (defn aaa [arr sum]
   (let [size (count arr) 
@@ -24,18 +41,7 @@
                  (count lst1)
                  (- size (count lst1))
                  "f=" f)
-        (let [f1 (first lst1)]
-          (loop [lst2 (rest lst1)]
-            (println " > pos1=" (- size (count lst1)) ;; 1-я позиция
-                     "pos2=" (- size (count lst2));; 2-я позиция
-                     )
-            (when (and lst2 
-                       (not= sum (+ f1 (first lst2))))
-              (println "- =" lst2
-                       (count lst2)
-                       "f1=" f1)
-              (recur (next lst2)))
-            ))
+        (bbb lst1 size sum)
         (recur (next lst1))))
     (when f
       (println
