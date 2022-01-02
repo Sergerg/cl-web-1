a6143dbc507676b670998e6c2196479ff548955a
Sergey Galichanin <sergerg@gmail.com>
2022-01-02 10:18:27 +0400

    первую итерацию проходит!

--- src/cl_easy/twosum.clj
+++ src/cl_easy/twosum.clj
@@ -1,17 +1,7 @@
 (ns cl-easy.twosum
   (:gen-class))
 
-;; first rest
-(defn t1 [arr sum]
-  (let [pos (count arr)])
-  )
-
-;; TODO: here
-(defn twosum [numbers target]
-  [1 1]
-  )
-
-(defn bbb [lst1 size sum]
+(defn chk-sum-log [lst1 size sum]
   (let [first1 (first lst1)]
     (loop [lst2 (rest lst1)]
       (let [pos1 (- size (count lst1))
@@ -19,35 +9,47 @@
             flag (not= sum (+ first1 (first lst2)))]
         (println "   > pos1=" pos1  ;; 1-я позиция
                  "pos2=" pos2     ;; 2-я позиция
-                 "flag=" flag
-                 )
-        (when (and lst2 flag)
-          (println "- =" lst2
-                   (count lst2)
-                   "first1=" first1)
-          (recur (next lst2)))))
-    ))
+                 "flag=" flag)
+        (println "  =" lst2
+                 (count lst2)
+                 "first1=" first1)
+        (if (and lst2 flag)
+          (recur (next lst2))
+          (vector pos1 pos2))))))
 
-;; TEST!
-(defn aaa [arr sum]
-  (let [size (count arr) 
-        f false
-        res []]
+;; 
+(defn twosum-log [arr sum]
+  (let [size (count arr)]
     (println "size=" size
              "sum=" sum)
     (loop [lst1 arr]
-      (when lst1
-        (println "-" lst1
-                 (count lst1)
-                 (- size (count lst1))
-                 "f=" f)
-        (bbb lst1 size sum)
-        (recur (next lst1))))
-    (when f
-      (println
-       "===" res)
-      res)
-    ))
+      (println "-" lst1
+               (count lst1)
+               (- size (count lst1)))
+      (let [res (chk-sum-log lst1 size sum)]
+        (println "- res=" res)
+        (if (and (not res) lst1)
+          (recur (next lst1))
+          (when res
+            res))))))
 
+(defn chk-sum [lst1 size sum]
+  (let [first1 (first lst1)]
+    (loop [lst2 (rest lst1)]
+      (let [pos1 (- size (count lst1))
+            pos2 (- size (count lst2))
+            flag (not= sum (+ first1 (first lst2)))]
+        (if (and lst2 flag)
+          (recur (next lst2))
+          (vector pos1 pos2))))))
 
+;; TODO: here
+(defn twosum [numbers target]
+  (let [size (count numbers)]
+    (loop [lst1 numbers]
+      (let [res (chk-sum lst1 size target)]
+        (if (and (not res) lst1)
+          (recur (next lst1))
+          (when res
+            res))))))
 
